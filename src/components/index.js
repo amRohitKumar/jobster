48b9de27ec5f4c7960af8f84ed3e0c61549835ff
Rohit Kumar <shutupRohit@gmail.com>
2022-07-31 20:44:56 +0530

    stats page completed

--- src/components/index.js
+++ src/components/index.js
@@ -7,6 +7,9 @@ import ProtectedRoutes from "./ProtectedRoutes";
 import FormRowSelect from "./FormRowSelect";
 import JobsContainer from "./JobsContainer";
 import SearchContainer from "./SearchContainer";
+import Loading from "./Loading";
+import StatsContainer from "./StatsContainer";
+import ChartsContainer from "./ChartsContainer";
 
 export {
   Logo,
@@ -18,4 +21,7 @@ export {
   FormRowSelect,
   SearchContainer,
   JobsContainer,
+  Loading,
+  StatsContainer,
+  ChartsContainer,
 };
