91d1ea5b486339af9ce3c5550a3b20bc10c3567f
Rohit Kumar <shutupRohit@gmail.com>
2022-12-14 17:42:23 +0530

    add client url in cors

--- server/index.js
+++ server/index.js
@@ -1,36 +1,36 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import authRoutes from './routes/auth.js';
-import jobRoutes from './routes/jobs.js';
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import authRoutes from "./routes/auth.js";
+import jobRoutes from "./routes/jobs.js";
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   dotenv.config();
 }
 const app = express();
 
 const PORT = process.env.PORT || 8080;
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/jobster';
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/jobster";
 
 mongoose
   .connect(dbUrl)
   .then(() => {
-    console.log('MONGOOSE CONNECTION OPEN');
+    console.log("MONGOOSE CONNECTION OPEN");
   })
   .catch((err) => {
-    console.log('IN MONGOOSE SOMETHING WENT WRONG', err);
+    console.log("IN MONGOOSE SOMETHING WENT WRONG", err);
   });
 
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
-  }),
+    origin: "https://jobster-project.netlify.app",
+  })
 );
 
-app.use('/auth', authRoutes);
-app.use('/jobs', jobRoutes);
+app.use("/auth", authRoutes);
+app.use("/jobs", jobRoutes);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
